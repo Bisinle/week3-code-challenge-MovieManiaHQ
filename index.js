39f0313523a6ab66a7072818d9365ccaf887b426
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-07-02 01:00:46 +0300

    pop up implimented

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const movieUL = document.querySelector("#films");
 movieUL.classList.add("movieUL");
 
-const movieDetails = document.querySelector("#popup");
-const moveTitle = document.querySelector("#moveTitle");
-const movieDescription = document.querySelector("#movieDescription");
-const movieLink = document.querySelector("#movieLink");
-
 ///////////////////////////////////////////////////////////////////
 fetch(`http://localhost:3000/films`)
   .then((res) => {
@@ -22,7 +17,7 @@ fetch(`http://localhost:3000/films`)
   });
 
 function movieCards(moveObject) {
-  console.log(moveObject);
+  console.log(moveObject[0]);
   for (const movie of moveObject) {
     const movieItem = document.createElement("li");
     movieItem.innerHTML = `
@@ -30,12 +25,19 @@ function movieCards(moveObject) {
 		<div class="card movie_card " id="card${movie.id}">
         <img src=
         "${movie.poster}"" alt="...">
+          <a id="details" href="#popup">
+               <i id="play" class="fa-solid  fa-circle-play"></i>
+               </a>
+
         <div class="card-body">
-            <i class="fas fa-play play_button" data-toggle="tooltip" data-placement="bottom" title="Play Trailer">
-            </i>
           <h5 class="card-title">${movie.title}</h5>
-                 <span class="movie_info">2019</span>
-                 <span class="movie_info float-right"><i class="fas fa-star"></i> 9 / 10</span>
+               <div id="spans">
+               <span class="movie_info">2019</span>
+               <span class="movie_info movie">Movie</span>
+               <span class="movie_info ">${movie.runtime} min</span>
+                </div>
+             
+              
         </div>
       </div>
 
@@ -44,15 +46,34 @@ function movieCards(moveObject) {
 
     movieUL.append(movieItem);
     const card = document.getElementById(`card${movie.id}`);
-    const linkId = document.getElementById(`popup${movie.id}`);
-    console.log(card);
+    // console.log(card);
+    const movieDetails = document.querySelector("#popup");
+    const movieTitle = document.querySelector("#movieTitle");
+    const movieDescription = document.querySelector("#movieDescription");
+    const movieLink = document.querySelector("#movieLink");
 
     card.addEventListener("click", () => {
-      // console.log(movie.title);
-      card.classList.add("active");
+      // card.classList.add("active");
+      movieUL.classList.add("active");
+      movieDetails.classList.add("active");
+
+      if (movieDetails.classList.contains("active")) {
+        document.body.style.overflow = "hidden";
+      } else {
+        body.style.overflow = "auto";
+      }
+      //grap the elemets on the popup and insert the content
+      movieTitle.textContent = ` ${movie.title}`;
+      movieDescription.textContent = `${movie.description}`;
+
+      console.log(card);
     });
     card.addEventListener("mouseleave", () => {});
-    card.classList.remove("active");
-    // movieDetails.classList.remove("active");
+
+    movieLink.addEventListener("click", () => {
+      card.classList.remove("active");
+      movieUL.classList.remove("active");
+      movieDetails.classList.remove("active");
+    });
   }
 }
